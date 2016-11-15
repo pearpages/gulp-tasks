824beb47f4144bd3c6354592bbe7e4b244ab2ac2
pearpages <pere@soms.cat>
2016-11-15 18:29:06 +0100

    gulp config file

---
+++ gulp/gulp.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    build_dir: './build',
+    app_files: {
+        tpl_src: ['./build/vendor/**/*.js',
+            './build/app/**/*.module.js',
+            './build/app/**/*.js',
+            '!./build/app/**/*.spec.js',
+            './build/assets/css/**/*.css'
+        ]
+    }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,26 @@ var runSequence = require('run-sequence');
 var del = require('del');
 var inject = require('gulp-inject');
 var serve = require('gulp-serve');
+var files = require('./gulp/gulp.config.js');
 
 gulp.task('default', function (callback) {
-    runSequence('build', callback);
+    runSequence('build', 'serve', callback);
 });
 
 gulp.task('build', function(callback) {
-    runSequence('clean', 'copy-build', 'index', 'serve', callback);
+    runSequence('clean', 'copy-build', 'index', callback);
 });
 
 gulp.task('clean', function() {
-    return del(['./build'], {force:true});
+    return del([files.build_dir], {force:true});
 });
 
 gulp.task('serve', serve('build'));
 
 gulp.task('index', function () {
-    var tpl_src = ['./build/vendor/**/*.js',
-    './build/app/**/*.module.js',
-    './build/app/**/*.js',
-    '!./build/app/**/*.spec.js',
-    './build/assets/css/**/*.css'
-    ];
     return gulp.src('./src/index.html')
-        .pipe(inject(gulp.src(tpl_src), {ignorePath: 'build'}))
-        .pipe(gulp.dest('./build'));
+        .pipe(inject(gulp.src(files.app_files.tpl_src), {ignorePath: 'build'}))
+        .pipe(gulp.dest(files.build_dir));
 });
 
 // dependencies are run before
@@ -35,15 +30,15 @@ gulp.task('copy-build', ['copy-assets', 'copy-app-js', 'copy-vendor-js']);
 
 gulp.task('copy-assets', function () {
     return gulp.src('./src/assets/**/*')
-        .pipe(gulp.dest('./build/assets'));
+        .pipe(gulp.dest(files.build_dir+'/assets'));
 });
 
 gulp.task('copy-app-js', function () {
         return gulp.src('./src/**/*.js')
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest(files.build_dir));
 });
 
 gulp.task('copy-vendor-js', function () {
     return gulp.src('./vendor/**/*.js')
-        .pipe(gulp.dest('./build/vendor'));
+        .pipe(gulp.dest(files.build_dir+'/vendor'));
 });
